0ce2fed7ac8dfbaad2fc724d0f577c8473fd25da
ilevak <37185799+ilevak@users.noreply.github.com>
2020-11-04 17:34:01 +0100

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -4,9 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *Ulazne recenice moraju biti odvojene ";" npr. Iva Levak; keavaliv
  */
 /**
  *
