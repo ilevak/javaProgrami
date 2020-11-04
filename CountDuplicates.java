96253912831b9176afc9f0886253a5f27d456d46
ilevak <37185799+ilevak@users.noreply.github.com>
2020-11-04 17:36:14 +0100

    Update CountDuplicates.java

--- CountDuplicates.java
+++ CountDuplicates.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ulazna linija: bilo koja recenica ili rijec.
+ * Prebrojava duplična slova unutar Stringa i ispisuje koliko se puta ponavljaju
  */
 
 /**
